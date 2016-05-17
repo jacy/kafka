a8c04a24b0c37c066cd3fb83a0128356a75352e9
jacy <johnsoncr.hong@gmail.com>
2016-05-17 15:32:18 +0800

    Use async to improve the throughput

--- src/main/java/jacy/kafka/Producer1.java
+++ src/main/java/jacy/kafka/Producer1.java
@@ -8,6 +8,7 @@ import kafka.producer.KeyedMessage;
 import kafka.producer.ProducerConfig;
 
 public class Producer1 {
+	private static final String NEVER_WAIT_ACKNOWLEDGE = "0";
 	private static final String BROKER_ENDPOINTS = "localhost:9092";
 	public static final String TOPIC = "mytopic";
 
@@ -15,14 +16,15 @@ public class Producer1 {
 		Properties props = new Properties();
 		props.put("metadata.broker.list", BROKER_ENDPOINTS);
 		props.put("serializer.class", "kafka.serializer.StringEncoder");
-		props.put("request.required.acks", "1");
+		props.put("request.required.acks", NEVER_WAIT_ACKNOWLEDGE);
+		props.put("producer.type", "async");
 
 		ProducerConfig config = new ProducerConfig(props);
 
 		Producer<String, String> producer = new Producer<String, String>(config);
 
 		Long start = System.currentTimeMillis();
-		for (long nEvents = 0; nEvents < 5; nEvents++) {
+		for (long nEvents = 0; nEvents < 50000; nEvents++) {
 			String msg = new Date().toString() + ", events >> " + nEvents;
 			KeyedMessage<String, String> data = new KeyedMessage<String, String>(TOPIC, "" + nEvents, msg);
 			producer.send(data);
